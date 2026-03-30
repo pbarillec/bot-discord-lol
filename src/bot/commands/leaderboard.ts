aca116d882b0ba726ecdaeac8f73efc2c8234b1c
Pierre Barillec <barillec.pierre@gmail.com>
2026-03-30 19:58:26 +0800

    feat: add leaderboard command stub

---
+++ src/bot/commands/leaderboard.ts
@@ -0,0 +1,5 @@
+import { ChatInputCommandInteraction } from "discord.js";
+
+export async function handleLeaderboardCommand(interaction: ChatInputCommandInteraction): Promise<void> {
+  await interaction.reply("Not implemented yet");
+}
