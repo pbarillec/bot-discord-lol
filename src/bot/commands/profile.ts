43eceb988389bd8b80647ec13bf815c2a7d03dc6
Pierre Barillec <barillec.pierre@gmail.com>
2026-03-30 19:58:17 +0800

    feat: add profile command stub

---
+++ src/bot/commands/profile.ts
@@ -0,0 +1,5 @@
+import { ChatInputCommandInteraction } from "discord.js";
+
+export async function handleProfileCommand(interaction: ChatInputCommandInteraction): Promise<void> {
+  await interaction.reply("Not implemented yet");
+}
