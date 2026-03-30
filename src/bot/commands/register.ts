fbf2e4e54e9dc7f78fa3cef0ccfd8097e3b2cc19
Pierre Barillec <barillec.pierre@gmail.com>
2026-03-30 19:57:49 +0800

    feat: add register command stub

---
+++ src/bot/commands/register.ts
@@ -0,0 +1,5 @@
+import { ChatInputCommandInteraction } from "discord.js";
+
+export async function handleRegisterCommand(interaction: ChatInputCommandInteraction): Promise<void> {
+  await interaction.reply("Not implemented yet");
+}
