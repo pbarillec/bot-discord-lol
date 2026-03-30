70bba2fc299a02f3c5b26e2519440ae0d6b2d84b
Pierre Barillec <barillec.pierre@gmail.com>
2026-03-30 20:16:13 +0800

    feat: add player type model

---
+++ src/types/player.ts
@@ -0,0 +1,10 @@
+export type Player = {
+  id: number;
+  discord_user_id: string;
+  discord_username: string;
+  riot_game_name: string;
+  riot_tag_line: string;
+  puuid: string;
+  region: string;
+  created_at: string;
+};
